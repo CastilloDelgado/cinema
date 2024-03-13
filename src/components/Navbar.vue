09439e7cbb369da16445de8cc1dc7f385b59b005
Marco Castillo <marcocastillo1997@gmail.com>
2024-03-12 18:58:56 -0600

    primer deploy

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,58 @@
 <script setup>
 import { ref } from 'vue';
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 
 const isOpen = ref(false)
 </script>
 
 <template>
     <div class="w-full bg-black sticky top-0 border-b-4 border-main-400 z-10">
-        <div
-            class="max-w-6xl mx-auto min-h-16 grid grid-cols-1 md:px-4 md:grid-cols-2 place-content-center md:place-content-around md:py-0">
-            <p class="text-center md:text-start font-main text-main-400 cursor-pointer md:mb-0" :class="{
-                'mb-3': isOpen
-            }">Cinema Concert Club
+        <div class="max-w-6xl mx-auto min-h-16 grid px-4 grid-cols-2 place-content-around md:py-0">
+            <p
+                class="text-center text-nowrap self-center md:text-start font-main text-main-400 text-sm  md:text-lg cursor-pointer md:mb-0">
+                Cinema Concert Club
             </p>
-            <div class="flex text-sm lg:text-base flex-col md:flex-row text-white justify-end gap-4 text-center md:text-start"
-                v-if="isOpen">
-                <p class="text-main-200 hover:text-white hover:underline cursor-pointer">Orquesta</p>
-                <p class="text-main-200 hover:text-white hover:underline cursor-pointer">Cine + Música en Vivo</p>
-                <p class="text-main-200 hover:text-white hover:underline cursor-pointer">Proximos eventos</p>
-                <p class="text-main-200 hover:text-white hover:underline cursor-pointer">Newsletter</p>
+            <div
+                class="capitalize text-sm lg:text-base flex-col md:flex-row text-white justify-end gap-4 text-center md:text-start hidden md:flex">
+                <p class="self-center text-main-200 hover:text-white hover:underline cursor-pointer">Orquesta</p>
+                <p class="self-center text-main-200 hover:text-white hover:underline cursor-pointer">Cine + Música en
+                    Vivo</p>
+                <p class="self-center text-main-200 hover:text-white hover:underline cursor-pointer">Próximos eventos
+                </p>
+                <p class="self-center text-main-200 hover:text-white hover:underline cursor-pointer">Newsletter</p>
+            </div>
+            <div class="flex md:hidden justify-end">
+                <Menu>
+                    <MenuButton>
+                        <div
+                            class="rounded-sm text-xs border-main-500 border-[1px] p-2 hover:bg-main-500 hover:text-black bg-black text-main-500 capitalize">
+                            Menu
+                        </div>
+                    </MenuButton>
+                    <MenuItems as="div"
+                        class="absolute top-16 bg-black text-main-400  rounded-sm px-2 py-3 gap-2 border-[1px] border-main-500 flex flex-col">
+                        <MenuItem v-slot="{ active }">
+                        <a :class='{ "text-white": active }' href="#">
+                            Orquesta
+                        </a>
+                        </MenuItem>
+                        <MenuItem v-slot="{ active }">
+                        <a :class='{ "text-white": active }' href="#">
+                            Cine + Música en Vivo
+                        </a>
+                        </MenuItem>
+                        <MenuItem v-slot="{ active }">
+                        <a :class='{ "text-white": active }' href="#">
+                            Próximos Eventos
+                        </a>
+                        </MenuItem>
+                        <MenuItem v-slot="{ active }">
+                        <a :class='{ "text-white": active }' href="#">
+                            Newsletter
+                        </a>
+                        </MenuItem>
+                    </MenuItems>
+                </Menu>
             </div>
         </div>
     </div>
